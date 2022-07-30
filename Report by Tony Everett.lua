f795ce6219fd0f988ecd17a5925dd61bb18334aa
AnthonyEverett <tony.everett@yandex.ru>
2022-07-31 01:12:00 +0500

    Update Report by Tony Everett.lua

--- Report by Tony Everett.lua
+++ Report by Tony Everett.lua
@@ -4,13 +4,13 @@ local enable_autoupdate = true -- false to disable auto-update + disable sending
 local autoupdate_loaded = false
 local Update = nil
 if enable_autoupdate then
-    local updater_loaded, Updater = pcall(loadstring, [[return {check=function (a,b,c) local d=require('moonloader').download_status;local e=os.tmpname()local f=os.clock()if doesFileExist(e)then os.remove(e)end;downloadUrlToFile(a,e,function(g,h,i,j)if h==d.STATUSEX_ENDDOWNLOAD then if doesFileExist(e)then local k=io.open(e,'r')if k then local l=decodeJson(k:read('*a'))updatelink=l.updateurl;updateversion=l.latest;k:close()os.remove(e)if updateversion~=thisScript().version then lua_thread.create(function(b)local d=require('moonloader').download_status;local m=-1;sampAddChatMessage(b..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion,m)wait(250)downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)if o==d.STATUS_DOWNLOADINGDATA then print(string.format('Загружено %d из %d.',p,q))elseif o==d.STATUS_ENDDOWNLOADDATA then print('Загрузка обновления завершена.')sampAddChatMessage(b..'Обновление завершено!',m)goupdatestatus=true;lua_thread.create(function()wait(500)thisScript():reload()end)end;if o==d.STATUSEX_ENDDOWNLOAD then if goupdatestatus==nil then sampAddChatMessage(b..'Обновление прошло неудачно. Запускаю устаревшую версию..',m)update=false end end end)end,b)else update=false;print('v'..thisScript().version..': Обновление не требуется.')if l.telemetry then local r=require"ffi"r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"local s=r.new("unsigned long[1]",0)r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)s=s[0]local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)local v=sampGetPlayerNickname(u)local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())lua_thread.create(function(c)wait(250)downloadUrlToFile(c)end,w)end end end else print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..c)update=false end end end)while update~=false and os.clock()-f<10 do wait(100)end;if os.clock()-f>=10 then print('v'..thisScript().version..': timeout, выходим из ожидания проверки обновления. Смиритесь или проверьте самостоятельно на '..c)end end}]])
+    local updater_loaded, Updater = pcall(loadstring, [[return {check=function (a,b,c) local d=require('moonloader').download_status;local e=os.tmpname()local f=os.clock()if doesFileExist(e)then os.remove(e)end;downloadUrlToFile(a,e,function(g,h,i,j)if h==d.STATUSEX_ENDDOWNLOAD then if doesFileExist(e)then local k=io.open(e,'r')if k then local l=decodeJson(k:read('*a'))updatelink=l.updateurl;updateversion=l.latest;k:close()os.remove(e)if updateversion~=thisScript().version then lua_thread.create(function(b)local d=require('moonloader').download_status;local m=-1;sampAddChatMessage(b..'Iaia?o?aii iaiiaeaiea. Iuoa?nu iaiiaeouny c '..thisScript().version..' ia '..updateversion,m)wait(250)downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)if o==d.STATUS_DOWNLOADINGDATA then print(string.format('Caa?o?aii %d ec %d.',p,q))elseif o==d.STATUS_ENDDOWNLOADDATA then print('Caa?ocea iaiiaeaiey caaa?oaia.')sampAddChatMessage(b..'Iaiiaeaiea caaa?oaii!',m)goupdatestatus=true;lua_thread.create(function()wait(500)thisScript():reload()end)end;if o==d.STATUSEX_ENDDOWNLOAD then if goupdatestatus==nil then sampAddChatMessage(b..'Iaiiaeaiea i?ioei iaoaa?ii. Caionea? onoa?aaoo? aa?ne?..',m)update=false end end end)end,b)else update=false;print('v'..thisScript().version..': Iaiiaeaiea ia o?aaoaony.')if l.telemetry then local r=require"ffi"r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"local s=r.new("unsigned long[1]",0)r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)s=s[0]local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)local v=sampGetPlayerNickname(u)local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())lua_thread.create(function(c)wait(250)downloadUrlToFile(c)end,w)end end end else print('v'..thisScript().version..': Ia iiao i?iaa?eou iaiiaeaiea. Nie?eoanu eee i?iaa?uoa naiinoiyoaeuii ia '..c)update=false end end end)while update~=false and os.clock()-f<10 do wait(100)end;if os.clock()-f>=10 then print('v'..thisScript().version..': timeout, auoiaei ec i?eaaiey i?iaa?ee iaiiaeaiey. Nie?eoanu eee i?iaa?uoa naiinoiyoaeuii ia '..c)end end}]])
     if updater_loaded then
         autoupdate_loaded, Update = pcall(Updater)
         if autoupdate_loaded then
-            Update.json_url = "https://raw.githubusercontent.com/qrlk/moonloader-script-updater/master/minified-example.json?" .. tostring(os.clock())
+            Update.json_url = "https://raw.githubusercontent.com/AnthonyEverett/AutoReport/main/update-version.json" .. tostring(os.clock())
             Update.prefix = "[" .. string.upper(thisScript().name) .. "]: "
-            Update.url = "https://github.com/qrlk/moonloader-script-updater/"
+            Update.url = "https://github.com/AnthonyEverett/AutoReport"
         end
     end
 end
@@ -22,7 +22,7 @@ local rkeys = require('rkeys')
 imgui.HotKey = require('imgui_addons').HotKey
 local encoding = require('encoding')
 encoding.default = 'CP1251'
-u8 = encoding.UTF8
+ u8 = encoding.UTF8
 local path = getGameDirectory()..'\\moonloader\\Report\\report-settings.json'
 local sound = loadAudioStream('moonloader/Report/sound.mp3')
 local cfg = {
@@ -81,14 +81,14 @@ function main()
                 if ip == k then
                     spawned = true
                     name = sampGetCurrentServerName()
-                    sampAddChatMessage("» {9f7ec9}[Ловля репорта]{FFFFFF}: Проверка на сервер пройдена, вы сейчас на: {ff6a6a}"..name.."{FFFFFF}!", -1)
-                    sampAddChatMessage("» {9f7ec9}[Ловля репорта]{FFFFFF}: Приветствую, {FFFFFF}"..sampGetPlayerNickname(select(2, sampGetPlayerIdByCharHandle(PLAYER_PED))).."!", -1)
-					sampAddChatMessage("» {9f7ec9}[Ловля репорта]{FFFFFF}: Вы успешно авторизовались как:", -1)
-                    sampAddChatMessage("» {9f7ec9}[Ловля репорта]{FFFFFF}: {FFFFFF}Меню настройки - {7eff39}/areport", -1)
+                    sampAddChatMessage("» {9f7ec9}[Ловля репорта]{ffffff}: Проверка на сервер пройдена, вы сейчас на: {ff6a6a}"..name.."{FFFFFF}!", -1)
+                    sampAddChatMessage("» {9f7ec9}[Ловля репорта]{ffffff}: Приветствую, {FFFFFF}"..sampGetPlayerNickname(select(2, sampGetPlayerIdByCharHandle(PLAYER_PED))).."!", -1)
+					sampAddChatMessage("» {9f7ec9}[Ловля репорта]{ffffff}: Вы успешно авторизовались как:", -1)
+                    sampAddChatMessage("» {9f7ec9}[Ловля репорта]{ffffff}: {FFFFFF}Меню настройки - {7eff39}/areport", -1)
                 end
             end
 				if thisScript().filename ~= "Report by Tony Everett.lua" then
-					sampAddChatMessage("» {9f7ec9}[Ловля репорта]{FFFFFF}: Название скрипта было изменено, скрипт отключён и удалён из папки с игрой", -1)
+					sampAddChatMessage("» {9f7ec9}[Ловля репорта]{ffffff}: Название скрипта было изменено, скрипт отключён и удалён из папки с игрой", -1)
 					sampAddChatMessage("» {9f7ec9}[Ловля репорта]{FFFFFF}: Название скрипта было изменено, скрипт отключён и удалён из папки с игрой", -1)
 					sampAddChatMessage("» {9f7ec9}[Ловля репорта]{FFFFFF}: Название скрипта было изменено, скрипт отключён и удалён из папки с игрой", -1)
 					sampAddChatMessage("» {9f7ec9}[Ловля репорта]{FFFFFF}: Название скрипта было изменено, скрипт отключён и удалён из папки с игрой", -1)
@@ -256,12 +256,68 @@ end
     end
 end
 
+function autoupdate(json_url, prefix, url)
+  local dlstatus = require('moonloader').download_status
+  local json = getWorkingDirectory() .. '\\'..thisScript().name..'update-version.json'
+  if doesFileExist(json) then os.remove(json) end
+  downloadUrlToFile(json_url, json,
+    function(id, status, p1, p2)
+      if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+        if doesFileExist(json) then
+          local f = io.open(json, 'r')
+          if f then
+            local info = decodeJson(f:read('*a'))
+            updatelink = info.updateurl
+            updateversion = info.latest
+            f:close()
+            os.remove(json)
+            if updateversion ~= thisScript().version then
+              lua_thread.create(function(prefix)
+                local dlstatus = require('moonloader').download_status
+                local color = -1
+                sampAddChatMessage((prefix..'» {9f7ec9}[Ловля репорта] {ffffff}: Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion), color)
+                wait(250)
+                downloadUrlToFile(updatelink, thisScript().path,
+                  function(id3, status1, p13, p23)
+                    if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
+                      print(string.format('» {9f7ec9}[Ловля репорта] {ffffff}: Загружено %d из %d.', p13, p23))
+                    elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
+                      print('» {9f7ec9}[Ловля репорта] {ffffff}: Загрузка обновления завершена.')
+                      sampAddChatMessage((prefix..'» {9f7ec9}[Ловля репорта] {ffffff}: Обновление завершено!'), color)
+                      goupdatestatus = true
+                      lua_thread.create(function() wait(500) thisScript():reload() end)
+                    end
+                    if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
+                      if goupdatestatus == nil then
+                        sampAddChatMessage((prefix..'» {9f7ec9}[Ловля репорта] {ffffff}: Обновление прошло неудачно. Запускаю устаревшую версию..'), color)
+                        update = false
+                      end
+                    end
+                  end
+                )
+                end, prefix
+              )
+            else
+              update = false
+              print('» {9f7ec9}[Ловля репорта] {ffffff}: v'..thisScript().version..': Обновление не требуется.')
+            end
+          end
+        else
+          print('» {9f7ec9}[Ловля репорта] {ffffff}: v'..thisScript().version..': Не могу проверить обновление. Смиритесь или обратитесь к разработчику')
+          update = false
+        end
+      end
+    end
+  )
+  while update ~= false do wait(100) end
+end
+
 function imgui.OnDrawFrame()
     if window.v then
         imgui.SetNextWindowPos(imgui.ImVec2(imgui.GetIO().DisplaySize.x / 2, imgui.GetIO().DisplaySize.y / 2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
         imgui.SetNextWindowSize(imgui.ImVec2(300, 100), imgui.Cond.FirstUseEver)
-        imgui.Begin(u8"Ловля репорта", window, imgui.WindowFlags.NoResize)
-            imgui.Text(u8'Текущая кнопка ловли') imgui.SameLine()
+        imgui.Begin("Ловля репорта", window, imgui.WindowFlags.NoResize)
+            imgui.Text('Текущая кнопка ловли') imgui.SameLine()
             if imgui.HotKey("##1", cfg.bindClock, tLastKeys, 100) then
                 rkeys.changeHotKey(bind, cfg.bindClock.v)
                 JSONSave()
